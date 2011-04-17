8fe791ae1c45506612b3591bd5a30b431543e960
Dan <dan@dc-home.ch>
2011-04-17 02:41:08 +0200

    Cleanup

--- src/lib/MiniMVC/orm/ORM.py
+++ src/lib/MiniMVC/orm/ORM.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-class ORM:
+class ORM(object):
 
     def __init__(self, type, host, user, password, database):
         self.type = type
